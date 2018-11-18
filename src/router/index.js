3e408494aa394fe8ebc3957a4eb993251d106acd
pengaice <email@example.com>
2018-11-18 15:01:16 +0800

    static pages

---
+++ src/router/index.js
@@ -0,0 +1,57 @@
+/*
+ 路由器对象模块
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import MSite from '../pages/MSite/MSite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
+
+// 声明使用vue插件
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history',
+  // 应用中所有路由
+  routes: [
+    {
+      path: '/msite',
+      component: MSite,
+      meta: {
+        showFoot: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFoot: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFoot: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFoot: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    }
+  ]
+})
